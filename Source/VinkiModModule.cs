f26f5574b99e4bb65fecb9637c8cef5592764f09
RepeatGitHub <charlietheodore@icloud.com>
2024-04-12 23:10:04 -0400

    Tried fixing bugs, might have added more? (Also working on new art spots)

--- Source/VinkiModModule.cs
+++ Source/VinkiModModule.cs
@@ -26,12 +26,13 @@ public class VinkiModModule : EverestModule {
     public static VinkiModSaveData SaveData => (VinkiModSaveData) Instance._SaveData;
 
     //but here's the :hunterglee: (the constants)
-    public static String[] textureNamespaces = ["scenery/car/body"];
-    public static String[] textureReplaceNamespaces = ["scenery/vinki/car/body"];
-    public static String[] textureUnReplaceNamespaces = ["scenery/madeline/car/body"];
-    public static String[] hasArtSpots = ["Celeste/0-Intro"];
+    public static String[] textureNamespaces = ["scenery/car/body","decals/1-forsakencity/big_sign_b","decals/1-forsakencity/camping_medium","decals/1-forsakencity/hanging_sign","decals/1-forsakencity/big_sign_e","decals/1-forsakencity/big_sign_d","decals/1-forsakencity/big_sign","decals/1-forsakencity/big_sign_c","scenery/memorial/memorial"];
+    public static String[] textureReplaceNamespaces = ["scenery/vinki/car/body","decals/vinki/big_sign_b","decals/vinki/camping_medium","decals/vinki/hanging_sign","decals/vinki/big_sign_e","decals/vinki/big_sign_d","decals/vinki/big_sign","decals/vinki/big_sign_c","scenery/vinki/memorial"];
+    public static String[] textureUnReplaceNamespaces = ["scenery/madeline/car/body","decals/madeline/big_sign_b","decals/madeline/camping_medium","decals/madeline/hanging_sign","decals/madeline/big_sign_e","decals/madeline/big_sign_d","decals/madeline/big_sign","decals/madeline/big_sign_c","scenery/madeline/memorial"];
+    public static String[] hasArtSpots = ["Celeste/0-Intro","Celeste/1-ForsakenCity"];
     public static int[][][] artSpots = [//x,y,w,h,textureNamespaces directory,tempx,tempy
-        [[-180,120,80,50,0,-192,135]]
+        [[-180,120,80,50,0,-192,134]],
+        [[1115,-1072,30,20,1,1104,-1083],[695,-1064,40,30,2,692,-1105],[1742,-1440,38,22,3,1740,-1456],[2233,-1344,40,66,4,2220,-1356],[2665,-1600,20,25,5,2664,-1612],[3340,-1950,70,35,6,3337,-1963],[3465,-2575,75,30,7,3456,-2579],[3985,-3140,40,80,8,3976,-3144]]
     ];
 
     public VinkiModModule() {
@@ -61,7 +62,10 @@ public class VinkiModModule : EverestModule {
         On.Celeste.IntroCar.Added -= introCarScrewery;
     }
 
-    private static void triggerVinkiGUI1(Level level, LevelData next, Microsoft.Xna.Framework.Vector2 direction) {vinkiGUI();}
+    private static void triggerVinkiGUI1(Level level, LevelData next, Microsoft.Xna.Framework.Vector2 direction) {
+        vinkiGUI();
+        graffitiSetup(level.Session);
+    }
     private static void triggerVinkiGUI2(Session session, bool fromSaveData) {
         vinkiGUI();
         graffitiSetup(session);
@@ -83,33 +87,34 @@ public class VinkiModModule : EverestModule {
             GFX.Gui["hover/idle"] = GFX.Game["Gui/hover/vinki/idle"];
             GFX.Game["pico8/atlas"] = GFX.Game["pico8/vinki/atlas"];
             GFX.Game["pico8/consolebg"] = GFX.Game["pico8/vinki/consolebg"];
-            GFX.Portraits["hug-light1"] = GFX.Game["Portraits/vinki/hug-light1"];
-            GFX.Portraits["hug-light2a"] = GFX.Game["Portraits/vinki/hug-light2a"];
-            GFX.Portraits["hug-light2b"] = GFX.Game["Portraits/vinki/hug-light2b"];
-            GFX.Portraits["hug-light2c"] = GFX.Game["Portraits/vinki/hug-light2c"];
-            GFX.Portraits["hug1"] = GFX.Game["Portraits/vinki/hug1"];
-            GFX.Portraits["hug2"] = GFX.Game["Portraits/vinki/hug2"];
+            GFX.Portraits["hug-light1"] = GFX.Portraits["vinki/hug-light1"];
+            GFX.Portraits["hug-light2a"] = GFX.Portraits["vinki/hug-light2a"];
+            GFX.Portraits["hug-light2b"] = GFX.Portraits["vinki/hug-light2b"];
+            GFX.Portraits["hug-light2c"] = GFX.Portraits["vinki/hug-light2c"];
+            GFX.Portraits["hug1"] = GFX.Portraits["vinki/hug1"];
+            GFX.Portraits["hug2"] = GFX.Portraits["vinki/hug2"];
         } else {
             GFX.Gui["hover/highlight"] = GFX.Game["Gui/hover/madeline/highlight"];
             GFX.Gui["hover/idle"] = GFX.Game["Gui/hover/madeline/idle"];
             GFX.Game["pico8/atlas"] = GFX.Game["pico8/madeline/atlas"];
             GFX.Game["pico8/consolebg"] = GFX.Game["pico8/madeline/consolebg"];
-            GFX.Portraits["hug-light1"] = GFX.Game["Portraits/madeline/hug-light1"];
-            GFX.Portraits["hug-light2a"] = GFX.Game["Portraits/madeline/hug-light2a"];
-            GFX.Portraits["hug-light2b"] = GFX.Game["Portraits/madeline/hug-light2b"];
-            GFX.Portraits["hug-light2c"] = GFX.Game["Portraits/madeline/hug-light2c"];
-            GFX.Portraits["hug1"] = GFX.Game["Portraits/madeline/hug1"];
-            GFX.Portraits["hug2"] = GFX.Game["Portraits/madeline/hug2"];
+            GFX.Portraits["hug-light1"] = GFX.Portraits["madeline/hug-light1"];
+            GFX.Portraits["hug-light2a"] = GFX.Portraits["madeline/hug-light2a"];
+            GFX.Portraits["hug-light2b"] = GFX.Portraits["madeline/hug-light2b"];
+            GFX.Portraits["hug-light2c"] = GFX.Portraits["madeline/hug-light2c"];
+            GFX.Portraits["hug1"] = GFX.Portraits["madeline/hug1"];
+            GFX.Portraits["hug2"] = GFX.Portraits["madeline/hug2"];
         }
     }
 
     private static void graffitiSetup(Session session) {
         var rand = new Random();
-        Session.vinkiRenderIt[3]=rand.Next(0,2);
+        Session.vinkiRenderIt[3]=rand.Next(0,3);
         if (SkinModHelperModule.GetPlayerSkinName(-1)=="Vinki_Scug") {
             for (var a=0;a<textureNamespaces.Length;a++) {
                 if (SaveData.settingsArtChanged[a]) {
                     GFX.Game[textureNamespaces[a]]=GFX.Game[textureReplaceNamespaces[a]];
+                    Logger.Log(LogLevel.Warn,"VinkiMod",a.ToString()+" "+textureNamespaces[a]+" "+textureReplaceNamespaces[a]);
                 }
             }
         } else {
@@ -155,9 +160,9 @@ public class VinkiModModule : EverestModule {
         if (SkinModHelperModule.GetPlayerSkinName(-1)=="Vinki_Scug") {
             self.Add(new GraffitiIndicator());
             self.Add(new GraffitiTemp());
-        }
-        if (Array.IndexOf(hasArtSpots,self.Session.Area.SID)==0) {
-            self.Session.LevelData.Entities[2].Values["depth"]=2;
+            if (Array.IndexOf(hasArtSpots,self.Session.Area.SID)==0) {
+                self.Session.LevelData.Entities[2].Values["depth"]=2;
+            }
         }
     }
     
